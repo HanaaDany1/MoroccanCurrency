3946a298287baabff380b1e8c15c3873d7115bf7
HanaaDany1 <96061242+HanaaDany1@users.noreply.github.com>
2022-06-12 23:11:58 +0100

    Update money.cpp

--- money.cpp
+++ money.cpp
@@ -1,46 +1,40 @@
-// C++ program to accept an amount
-// and count number of notes
+/// C++ program to inpute amount of Dirhams and Cents 
+// and count number needed to achieve it
 #include <bits/stdc++.h>
 using namespace std;
 
 
-
-//int Paperbills[8] = { 200, 100, 50, 20,10, 5, 2, 1};
-double CentimeCoins[3]= {0.1, 0.2, 0.5 };
-
+int Paperbills[8] = { 200, 100, 50, 20, 10, 5, 2, 1};// number of Dirhams bills and coins we have in Moroccan Currency
+double CentimeCoins[3]= {0.1, 0.2, 0.5 };  //number of centimes coins we have since they are Double not integers
 
 
 
+void Dmoney(int Dirhams){
 
-void Dmoney(int Dirhams, int y, int MyDirhams){
-
-	int BillCountedSet[y] = { 0 };
-
-	
-	// count notes using Greedy approach
-	for (int i = 0; i < y ; i++) {
-		if (Dirhams > BillCountedSet[i]) {
-			//BillCountedSet[i] = Dirhams / Paperbills[i];
-		//	Dirhams = Dirhams - BillCountedSet[i] * Paperbills[i];
-        BillCountedSet[i] = Dirhams / MyDirhams[i];
-            Dirhams = Dirhams - BillCountedSet[i] * MyDirhams[i];
-		}
-	}
+	int BillCountedSet[8] = { 0 };
 	cout << "Dirhams Count  is -> " << endl;
-	for (int i = 0; i < y; i++) {
+	// count notes using the Greedy approach
+	for (int i = 0; i <8 ; i++) {
+		if (Dirhams > BillCountedSet[i]) {
+			BillCountedSet[i] = Dirhams / Paperbills[i];
+			Dirhams = Dirhams - BillCountedSet[i] * Paperbills[i];
+       
+	}
+	
+
+        
 		if (BillCountedSet[i] != 0) {
-		//	cout << Paperbills[i] << " : " << BillCountedSet[i] << endl;
-            	cout << MyDirhams[i] << " : " << BillCountedSet[i] << endl;
+		cout << Paperbills[i] << " : " << BillCountedSet[i] << endl;
+            	
 		}
 	}
 }
 
 void Cntmoney(double Cents)
 {
-
 	double BillCountedSet[3] = { 0 };
 	
-	// count notes using Greedy approach
+	// count the coins using same approach
 	for (int i = 0; i <  3; i++) {
 		if (Cents > BillCountedSet[i]) {
 			BillCountedSet[i] = Cents / CentimeCoins[i];
@@ -55,56 +49,19 @@ void Cntmoney(double Cents)
 	}
 }
 
-// Driver function
+
+
+
 int main()
 {
 
-
-
+	double Cents = 0.1;
+ 	int Dirhams = 6000;
+	Dmoney(Dirhams);
+        Cntmoney(Cents);
    
-//double MyCentimes[10];
-cout << "Please enter the number of Dirhams bills/coins you have on you"<< endl;
-int numBills;
-
-cin >> numBills;
-
-  int *MyDirhams = new int[numBills] ;
-  int total;
-for (int x = 0; x < numBills; i++) {
-    cout << "Enter each Bill/Coin seperatly" << endl;
-    cin >> MyDirhams[x];
-    total += MyDirhams[x];    
-}
-
-
-	double Cents = 0.7;
-    int Dirhams = 190;
-	
-  //  Cntmoney(Cents);
-    Dmoney(Dirhams, numBills);
 
  
-   int numBills;
-//double MyCentimes[10];
-cout << "Please enter the number of Dirhams bills/coins you have on you"<< endl;
-
-    cin >> numBills;
-
-  int *MyDirhams = new int[numBills] ;
-  int total;
-for (int i = 0; i < numBills; i++) {
-    cout << "Enter each Bill/Coin seperatly" << endl;
-    cin >> MyDirhams[i];
-    total += MyDirhams[i];    
-}
-
-
-//cout << "Please enter the centimes you have on you"<< endl;
-//cin >> MyCentimes[10];
-
-
-    //cout<< MyCentimes;
-   
-    
+ 
 	return 0;
 }
